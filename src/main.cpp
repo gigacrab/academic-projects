f28f1c210fa3db10abdbf884a46789817ea42d2b
gigacrab <tehwenliang@gmail.com>
2025-10-28 11:49:24 +0800

    updated 10s

--- src/main.cpp
+++ src/main.cpp
@@ -14,51 +14,44 @@ const int IN4 = 1;
 typedef struct {
   byte speed;
   struct {
-    byte forward;
-    byte backward;
+    boolean forward;
+    boolean backward;
   } direction;
 } motor; 
 
 motor motorA, motorB;
 
-int now;
-int interval = 10*1000; // 10 seconds
+unsigned long timeNow;
+unsigned long timeElapsed;
+int timeStart;
+unsigned int timeInterval = 10*1000; // 10 seconds
+
+boolean start;
 
 void setSpeed(byte speed){
-  motorA.speed = speed;
-  motorB.speed = speed;
+  motorA.speed = motorB.speed = speed;
   analogWrite(ENA, motorA.speed);
   analogWrite(ENB, motorB.speed);
 }
 
-void goBackward(){
-  motorA.direction.backward = HIGH;
-  motorA.direction.forward = LOW;
-  motorB.direction.backward = HIGH;
-  motorB.direction.forward = LOW;
+void go(String direction){
+  boolean value1 = LOW;
+  boolean value2 = LOW;
+  if (direction == "forward"){
+    value1 = LOW;
+    value2 = HIGH;
+  } else if (direction == "backward"){
+    value1 = HIGH;
+    value2 = LOW;
+  } else if (direction == "stop"){
+    setSpeed(0);
+  }
+  motorA.direction.backward = motorB.direction.backward = value1;
+  motorB.direction.forward = motorA.direction.forward = value2;
   digitalWrite(IN1, motorA.direction.forward);
   digitalWrite(IN2, motorA.direction.backward);
   digitalWrite(IN3, motorB.direction.forward);
   digitalWrite(IN4, motorB.direction.backward);
-}
-
-void goForward(){
-  motorA.direction.backward = LOW;
-  motorA.direction.forward = HIGH;
-  motorB.direction.backward = LOW;
-  motorB.direction.forward = HIGH;
-  digitalWrite(IN1, motorA.direction.forward);
-  digitalWrite(IN2, motorA.direction.backward);
-  digitalWrite(IN3, motorB.direction.forward);
-  digitalWrite(IN4, motorB.direction.backward);
-}
-
-void stop(){
-  setSpeed(0);
-  motorA.direction.backward = LOW;
-  motorA.direction.forward = LOW;
-  motorB.direction.backward = LOW;
-  motorB.direction.forward = LOW;
 }
 
 void setup() {
@@ -73,13 +66,25 @@ void setup() {
 }
 
 void loop() {
-  now = millis();
-  if (now <= interval){
-    setSpeed(100);
-    goForward();
-  } else {
-    stop();
-  }
+  start = true;
   lcd.setCursor(0, 0);
-  lcd.print(String(now/1000.0));
+  lcd.print("Select to start!");
+  while(!(analogRead(A0) > 600 && analogRead(A0) < 800));
+  timeStart = millis();
+  lcd.clear();
+  lcd.print("Moving forward!");
+
+  while(start){
+    timeNow = millis();
+    timeElapsed = timeNow - timeStart;
+    if (timeElapsed <= timeInterval){
+      setSpeed(100);
+      go("forward");
+    } else {
+      go("stop");
+      start = false;
+    }
+    lcd.setCursor(0, 1);
+    lcd.print("Time: " + String(timeElapsed/1000.0));
+  }
 }
