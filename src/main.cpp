f93100ce519b4eab1d609803a0fd685bd2be6fbb
gigacrab <tehwenliang@gmail.com>
2025-11-18 12:59:10 +0800

    added line following and encoder

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <PinChangeInterrupt.h>
 
 void INT0_ISR(void);
-void go(byte, byte);
+void go(int, int);
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7); // RS, E, D4, D5, D6, D7
 
@@ -20,7 +20,7 @@ const int ENCODER_R = 2;
 const int ENCODER_L = A3;
 
 const int IR_L = A5;
-const int IR_M = 1;
+//const int IR_M = 1;
 const int IR_R = A4;
 
 volatile uint8_t previousPortCState = 0;
@@ -38,8 +38,10 @@ boolean start;
 
 int distance;
 
-byte defaultSpeedL = 107;
-byte motorspeedR = 150;
+int defaultSpeedL = 100; // 107
+int defaultSpeedR = 100; // 150
+
+enum dir {STRAIGHT, LEFT, RIGHT};
 
 void setup() {
   lcd.begin(16, 2);
@@ -53,7 +55,7 @@ void setup() {
   pinMode(IN4, OUTPUT);
 
   pinMode(IR_R, INPUT);
-  pinMode(IR_M, INPUT);
+  //pinMode(IR_M, INPUT);
   pinMode(IR_L, INPUT);
 
   pinMode(ENCODER_R, INPUT);
@@ -67,6 +69,7 @@ void setup() {
 }
 
 void loop() {
+  enum dir lastDir = STRAIGHT;
   if (analogRead(A0) > 600 && analogRead(A0) < 800){
     millisStart = millis();
     lcd.clear();
@@ -77,7 +80,7 @@ void loop() {
       millisNow = millis();
       millisElapsed = millisNow - millisStart;
       
-      boolean L = digitalRead(IR_L);
+      /*boolean L = digitalRead(IR_L);
       boolean M = digitalRead(IR_M);  
       boolean R = digitalRead(IR_R);
 
@@ -101,6 +104,41 @@ void loop() {
           break;
         default:
           go(0, 0);
+      }*/
+
+      boolean L = digitalRead(IR_L);  
+      boolean R = digitalRead(IR_R);
+      
+      byte pattern = (L << 1) | R;
+
+      switch(pattern){
+        case 0b11:
+          switch(lastDir){
+            case STRAIGHT:
+              go(defaultSpeedL, defaultSpeedR);
+              lastDir = STRAIGHT;
+              break;
+            case LEFT:
+              go(-150, 255);
+              break;
+            case RIGHT:
+              go(255, -150);
+              break;
+          }
+          break;
+        case 0b10:
+          go(255, 200);
+          lastDir = RIGHT;
+          break;
+        case 0b01:
+          go(200, 255);
+          lastDir = LEFT;
+          break;
+        case 0b00:
+          go(0, 0);
+          lastDir = STRAIGHT;
+          start = false;
+          break;
       }
 
       lcd.setCursor(0, 1);  
@@ -108,11 +146,14 @@ void loop() {
     }
     start = true;
     lcd.setCursor(0, 0);
-    lcd.print("Select to start!");
-    lcd.setCursor(0, 1);
+    //lcd.print("Select to start!");
+    //lcd.setCursor(0, 1);
     lcd.print("Distance: ");
     distance = (pulseCountRight + pulseCountLeft) / 2 * M_PI * 6.5 / 20;
+    Serial.println(pulseCountLeft);
+    Serial.println(pulseCountRight);
     lcd.print(distance);
+    lcd.print("cm   ");
     pulseCountRight = 0;
     pulseCountLeft = 0;
   }
@@ -125,7 +166,7 @@ ISR(PCINT1_vect){
   if (!(previousPortCState & (1 << 3)) && (currentPortCState  & (1 << 3))) {
     microNowLeft = micros();
 
-    if ((microNowLeft - prevMicroLeft) * 10 > prevSampleLeft * 6) {
+    if ((microNowLeft - prevMicroLeft) > 0) {
         prevSampleLeft = microNowLeft - prevMicroLeft;
         prevMicroLeft = microNowLeft;
         pulseCountLeft++;
@@ -136,14 +177,14 @@ ISR(PCINT1_vect){
 
 void INT0_ISR(void){
   microNowRight = micros();
-  if (microNowRight - prevMicroRight > prevSampleRight * 0.6){
+  if (microNowRight - prevMicroRight > 0){
     prevSampleRight = microNowRight - prevMicroRight; 
     prevMicroRight = microNowRight;
     pulseCountRight++;
   }
 }
 
-void go(byte speedL, byte speedR){
+void go(int speedL, int speedR){ 
   digitalWrite(IN1, (speedR <= 0)? LOW : HIGH);
   digitalWrite(IN2, (speedR >= 0)? LOW : HIGH);
 
